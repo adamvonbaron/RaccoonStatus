25c8e4f31aba9853b91a03dc74e5f07eddfcc1d2
adam <apk@hey.com>
2022-01-30 13:59:40 -0500

    get ready to deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
@@ -46,8 +45,6 @@ Rails.application.configure do
     api_token: Rails.application.credentials.postmark_api_token
   }
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.perform_deliveries = true
-
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
