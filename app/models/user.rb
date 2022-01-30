48cfcf17046a246bf83da28263c937b672e6d4b2
adam <apk@hey.com>
2022-01-29 20:38:51 -0500

    support photos my man

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
 
-  has_many :statuses
+  has_many :statuses, dependent: :destroy
   has_one :current_status, -> { order(created_at: :desc) }, class_name: "Status"
+  
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [64, 64]
+  end
 
   def to_param = username
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations]
+  devise_for :users, path_names: { sign_up: "" }, controllers: {
+    registrations: "users/registrations"
+  }
 
   authenticate do
     resources :statuses
